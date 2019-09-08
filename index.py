25342011e76358033dfc786e3081bac1655ac22b
Davis Haupt <davis.haupt@gmail.com>
2019-09-08 16:41:21 -0400

    s p a c e s

--- index.py
+++ index.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "Welcome to PennClubReview!"
+    return "Welcome to Penn Club Review!"
 
 @app.route('/api')
 def api():
-    return "Welcome to the PennClubReview API!."
+    return "Welcome to the Penn Club Review API!."
 
 if __name__ == '__main__':
     app.run()
